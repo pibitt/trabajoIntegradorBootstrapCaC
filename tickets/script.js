b5f717a4d3dc4a4a581e6475c3631f158260ee68
pibitt <pabloroquez@gmail.com>
2022-10-18 11:35:17 -0300

    algunos cambios

--- tickets/script.js
+++ tickets/script.js
@@ -6,24 +6,29 @@ buttonSubmit.addEventListener("click", calcularPrecio);
 let buttonBorrar = document.getElementById('borrar');
 buttonBorrar.addEventListener('click', borrarInfo)
 
+
+//calcula el precio dependiendo de la cantidad de entradas y el tipo
 function calcularPrecio() {
     let cantidadTickets = document.getElementById("cantidadTickets").value;
     let tipoTicket = document.getElementById("tipoTicket").value;
+    let precioFull = 200;
     let precioTicket;
 
     if (tipoTicket === "Estudiante") {
-        precioTicket = 200 * 0.2 * cantidadTickets;
+        precioTicket = precioFull * 0.2 * cantidadTickets;
 
     } else if (tipoTicket === "trainee") {
-        precioTicket = 200 * 0.5 * cantidadTickets;
+        precioTicket = precioFull * 0.5 * cantidadTickets;
 
-    } else { precioTicket = 200 * 0.85 * cantidadTickets; }
+    } else { precioTicket = precioFull * 0.85 * cantidadTickets; }
 
 
     document.getElementById("total").value = "Total a pagar: $" + precioTicket;
 
 }
 
+
+//borra los campos
 function borrarInfo() {
     let inputs = document.querySelectorAll('input');
     for (let i = 0; i < inputs.length; i++) {
